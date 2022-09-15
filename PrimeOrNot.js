c94289161324a79167ec3258c47f2689a6cfc5f3
shailesh2137 <55477328+shailesh2137@users.noreply.github.com>
2022-09-15 09:22:42 +0530

    Create PrimeOrNot.js

---
+++ PrimeOrNot.js
@@ -0,0 +1,17 @@
+/* Check Whether a number is Prime or Not */
+for(let i=1;i<=5;i++)
+  {
+    let count=0;
+
+    for(let j=1;j<=i;j++)
+      {
+        if(i%j==0)
+        {
+          count++;
+        }
+      }
+    if(count==2)
+    {
+      console.log(i,"Prime");
+    }
+   
